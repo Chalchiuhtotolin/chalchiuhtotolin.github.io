3439541c0c39afc5a6046e5d887a420c9f383f0b
Chalchiuhtotolin <50812058+Chalchiuhtotolin@users.noreply.github.com>
2024-10-30 18:36:40 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,68 +2,58 @@
 const data = [
     {
         name: "Bossdog",
+        stats: ['16st', '5,11'],
         ratings: ["4", "3", "4", "3"],
-        image: "./static/star.png"
     },
     {
         name: "Dick Wilde",
+        stats: ['12st', '5,0'],
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Hincey",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Bobo",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Bendell",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Northern",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Tony",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Heinz",
         ratings: ["8", "10", "8", "9"],
-        image: "./static/star.png"
     },
     {
         name: "LookeTB",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
     {
         name: "Nicksy",
         ratings: ["?", "?", "?", "?"],
-        image: "./static/star.png"
     },
 ];
 
 const container = document.querySelector('.pitch');
 
 data.forEach(player => {
-    const box = document.createElement('div');
-    box.classList.add('box');
 
     const card = document.createElement('div');
     card.classList.add('card');
 
-    const img = document.createElement('img');
-    img.src = './static/shield.png';
-    img.classList.add('shield');
+    const shield = document.createElement('img');
+    shield.src = './static/shield.png';
+    shield.classList.add('shield');
 
     const portrait = document.createElement('img');
     portrait.src = "./static/turkey.png";
@@ -81,45 +71,60 @@ data.forEach(player => {
     nameDiv.classList.add('name');
     nameDiv.textContent = player.name;
 
-    const statsDiv = document.createElement('div');
-    statsDiv.classList.add('stats');
-
-    const events = ["CLAY", "GOLF", "POOL", "FTBL"]
-    const eventsDiv = document.createElement('div');
-    eventsDiv.classList.add('events');
-    events.forEach(event => {
-        const eventDiv = document.createElement('div');
-        eventDiv.classList.add('event');
-        eventDiv.textContent = event;
-        eventsDiv.appendChild(eventDiv);
-    });
-
-    const ratingsDiv = document.createElement('div');
-    ratingsDiv.classList.add('ratings');
-    player.ratings.forEach(() => {
-        const ratingDiv = document.createElement('div');
-        ratingDiv.classList.add('rating');
-        ratingDiv.textContent = "?";
-        ratingsDiv.appendChild(ratingDiv);
-    });
-
-    statsDiv.appendChild(eventsDiv);
-    statsDiv.appendChild(ratingsDiv);
-    card.appendChild(img);
+    card.appendChild(shield);
     card.appendChild(portrait);
     card.appendChild(overallDiv);
     card.appendChild(nameDiv);
-    card.appendChild(statsDiv);
+
+
+    function generate(titles, values, cls) {
+
+        const statsDiv = document.createElement('div');
+        statsDiv.classList.add('stats');
+        statsDiv.classList.add(cls);
+
+        const titlesDiv = document.createElement('div');
+        titlesDiv.classList.add('titles');
+        titles.forEach(title => {
+            const titleDiv = document.createElement('div');
+            titleDiv.classList.add('title');
+            titleDiv.textContent = title;
+            titlesDiv.appendChild(titleDiv);
+        });
+
+        const valuesDiv = document.createElement('div');
+        valuesDiv.classList.add('values');
+        values.forEach((value) => {
+            const valueDiv = document.createElement('div');
+            valueDiv.classList.add('value');
+            valueDiv.textContent = value;
+            valuesDiv.appendChild(valueDiv);
+        });
+
+        statsDiv.appendChild(titlesDiv);
+        statsDiv.appendChild(valuesDiv);
+        return statsDiv
+    }
+
+    const eventsDiv = generate(["CLAY", "GOLF", "POOL", "FTBL"], player.ratings, 'std')
+    eventsDiv.style.display = 'none';
+    card.appendChild(eventsDiv);
+
+    if (player.stats) {
+        const attributeDiv = generate(["WEIGHT", "HEIGHT"], player.stats, 'atr')
+        card.appendChild(attributeDiv);
+    }
+
+    const box = document.createElement('div');
+    box.classList.add('box');
 
     box.onmouseenter = function () {
         this.classList.add('flipped');
-
         setTimeout(() => {
-            portrait.src = player.image
+            portrait.src = "./static/star.png" //"./static/" + player.name + ".png"
             overallDiv.textContent = roundedMean;
-            player.ratings.forEach((rating, index) => {
-                ratingsDiv.children[index].textContent = rating;
-            });
+            card.querySelector('.std').style.display = 'block';
+            card.querySelector('.atr').style.display = 'none';
         }, 400);
     };
 
@@ -127,11 +132,10 @@ data.forEach(player => {
         this.classList.add('flipped');
 
         setTimeout(() => {
-            portrait.src = "./static/turkey.png"
+            portrait.src = "./static/turkey.png";
             overallDiv.textContent = '?';
-            player.ratings.forEach((_, index) => {
-                ratingsDiv.children[index].textContent = "?";
-            });
+            card.querySelector('.std').style.display = 'none';
+            card.querySelector('.atr').style.display = 'block';
         }, 400);
     };
 
